9f73bbe1f10b2fb3c7bc8f2be5505478382ca311
Bruno Moscuzza <122223456+bruno-m-dev@users.noreply.github.com>
2025-04-29 01:44:57 +0200

    Correcion checkboxes
    ya no quedan seleccionados si se elige otro en su lugar

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -12,27 +12,45 @@ var quantityInt: int = 0
 func _on_cb_one_cent_pressed() -> void:
 	coinValue = 0.01
 	play_sound($UiPop)
+	#Desmarca cualquier checkbox que esté marcado:
+	desmarcar_checkboxesCoins()
+	#Selecciona esta checkbox:
+	$VBoxContainer/CoinsGrid/CBOneCent.set_pressed_no_signal(true)
 func _on_cb_two_cent_pressed() -> void:
 	coinValue= 0.02
 	play_sound($UiPop)
+	desmarcar_checkboxesCoins()
+	$VBoxContainer/CoinsGrid/CBTwoCent.set_pressed_no_signal(true)
 func _on_cb_five_cent_pressed() -> void:
 	coinValue= 0.05
 	play_sound($UiPop)
+	desmarcar_checkboxesCoins()
+	$VBoxContainer/CoinsGrid/CBFiveCent.set_pressed_no_signal(true)
 func _on_cb_ten_cent_pressed() -> void:
 	coinValue = 0.10
 	play_sound($UiPop)	
+	desmarcar_checkboxesCoins()
+	$VBoxContainer/CoinsGrid/CBTenCent.set_pressed_no_signal(true)
 func _on_cb_twenty_cent_pressed() -> void:
 	coinValue = 0.20
 	play_sound($UiPop)
+	desmarcar_checkboxesCoins()
+	$VBoxContainer/CoinsGrid/CBTwentyCent.set_pressed_no_signal(true)
 func _on_cb_fifty_cent_pressed() -> void:
 	coinValue = 0.50
 	play_sound($UiPop)
+	desmarcar_checkboxesCoins()
+	$VBoxContainer/CoinsGrid/CBFiftyCent.set_pressed_no_signal(true)
 func _on_cb_one_euro_pressed() -> void:
 	coinValue = 1.00
 	play_sound($UiPop)
+	desmarcar_checkboxesCoins()
+	$VBoxContainer/CoinsGrid/CBOneEuro.set_pressed_no_signal(true)
 func _on_cb_two_euro_pressed() -> void:
 	coinValue = 2.00
 	play_sound($UiPop)
+	desmarcar_checkboxesCoins()
+	$VBoxContainer/CoinsGrid/CBTwoEuro.set_pressed_no_signal(true)
 #endregion
 	
 #ACTIVACION DE CHECK BOXES BILLETES
@@ -41,21 +59,33 @@ func _on_cb_two_euro_pressed() -> void:
 func _on_cb_five_euro_pressed() -> void:
 	coinValue = 5.00
 	play_sound($UiPop)
+	desmarcar_checkboxesBills()
+	$VBoxContainer/BillGrid/CBFiveEuro.set_pressed_no_signal(true)
 func _on_cb_ten_euro_pressed() -> void:
 	coinValue = 10.00
 	play_sound($UiPop)
+	desmarcar_checkboxesBills()
+	$VBoxContainer/BillGrid/CBTenEuro.set_pressed_no_signal(true)
 func _on_cb_twenty_euro_pressed() -> void:
 	coinValue = 20.00
 	play_sound($UiPop)
+	desmarcar_checkboxesBills()
+	$VBoxContainer/BillGrid/CBTwentyEuro.set_pressed_no_signal(true)
 func _on_cb_fifty_euro_pressed() -> void:
 	coinValue = 50.00
 	play_sound($UiPop)
+	desmarcar_checkboxesBills()
+	$VBoxContainer/BillGrid/CBFiftyEuro.set_pressed_no_signal(true)
 func _on_cb_one_hundred_euro_pressed() -> void:
 	coinValue = 100.00
 	play_sound($UiPop)
+	desmarcar_checkboxesBills()
+	$VBoxContainer/BillGrid/CBOneHundredEuro.set_pressed_no_signal(true)
 func _on_cb_two_hundred_euro_pressed() -> void:
 	coinValue = 200.00
 	play_sound($UiPop)
+	desmarcar_checkboxesBills()
+	$VBoxContainer/BillGrid/CBTwoHundredEuro.set_pressed_no_signal(true)
 #endregion
 	
 #ACTIVACION DE BOTONES NUMPAD (BOTONES CALCULADORA)
@@ -104,10 +134,19 @@ func _on_suma_button_pressed() -> void:
 	quantityInt = int(quantity)
 	#MULTIPLICA EL VALOR DE COIN/BILL ACTUAL POR LA CANTIDAD
 	subTotal = coinValue * quantityInt
-	
+		
 	#ACUMULA EL IMPORTE PARCIAL A LA ESPERA DE IR AGREGANDO MÁS
 	contador = contador + subTotal
-	
+	if contador >0.01 and contador <=50:
+		msg_sigue_asi()
+	if contador > 50 and contador <=100 :
+		msg_ahorra_mas()
+	elif contador >100 and contador <=500:
+		msg_buen_ahorro() 
+	elif contador >500 and contador <=1000 :
+		msg_papa_rico()
+	elif contador >1000 :
+		msg_robo_banco()
 	
 	#INFORMA DEL RESULTADO. TO/DO MOSTRAR ESTO MISMO EN LA APP
 	#antes, identifica si se trata de una moneda o billete para adecuar el mensaje que mostrará.
@@ -148,26 +187,54 @@ func reiniciar_variables():
 	quantity = ""
 	
 # MENSAJES	
+func msg_sigue_asi():
+	var msg: String = ("Tus primeros ahorros. Sigue así, ¡de mica en mica s'omple la pica!")
+	print (msg)
+	$VBoxContainer/Msg03.text = msg	
+func msg_ahorra_mas():
+	var msg: String = ("Ánimo, ya tienes algo ahorrado. ¡A por el siguiente logro!")
+	print (msg)
+	$VBoxContainer/Msg03.text = msg
+func msg_buen_ahorro():
+	var msg: String = ("¡Felicidades! ¡Has logrado reunir una buena cantidad!")
+	print (msg)
+	$VBoxContainer/Msg03.text = msg
+func msg_papa_rico():
+	var msg: String = ("¡Vaya, cuánta pasta tienes! ¿Te apellido es Barilla?")
+	print (msg)
+	$VBoxContainer/Msg03.text = msg
+func msg_robo_banco():
+	var msg: String = ("¡Pero bueno! ¿Acaso has robado un banco?   ¡¡¡@Policía!!!")
+	print (msg)
+	$VBoxContainer/Msg03.text = msg	
+
+	
+	
+	
+	
 func msg_cent_coins():
 	#los convierte a entero multiplicando por 100 para que se vea mejor el mensaje
 	var centimos = int(coinValue * 100)
-	var msg1: String = ("\nHas sumado " + str(quantity) + " monedas de " + str(centimos) + " céntimos")
-	var msg2: String = ("Llevas ahorrado " + str(contador) + " Euros " )
+	var total : float = (float(quantity)* float(coinValue))
+	var msg1: String = ("\nHas sumado " + str(quantity) + " monedas de " + str(centimos) + " céntimos = " + str(total) + " Euros.")
+	var msg2: String = ("Llevas ahorrado " + str(contador) + " Euros!! " )
 	print (msg1)
 	print (msg2)
 	$VBoxContainer/Msg01.text = msg1
 	$VBoxContainer/Msg02.text = msg2
 	
 func msg_euro_coins():
-	var msg1: String = ("\nHas sumado " + str(quantity) + " monedas de " + str(coinValue) + " Euro" )
-	var msg2: String = ("Llevas ahorrado " + str(contador) + " Euros " )
+	var total : float = (float(quantity)* float(coinValue)) 
+	var msg1: String = ("\nHas sumado " + str(quantity) + " monedas de " + str(coinValue) + " Euro = " + str(total) + " Euros.")
+	var msg2: String = ("Llevas ahorrado " + str(contador) + " Euros!! " )
 	print (msg1)
 	print (msg2)
 	$VBoxContainer/Msg01.text = msg1
 	$VBoxContainer/Msg02.text = msg2
 func msg_euro_bills():
-	var msg1: String = ("\nHas sumado " + str(quantity) + " billetes de " + str(coinValue) + " Euros" )
-	var msg2: String = ("Llevas ahorrado " + str(contador) + " Euros " )
+	var total : float = (float(quantity)* float(coinValue))
+	var msg1: String = ("\nHas sumado " + str(quantity) + " billetes de " + str(coinValue) + " Euros = " + str(total) + " Euros." )
+	var msg2: String = ("Llevas ahorrado " + str(contador) + " Euros!! " )
 	print (msg1)
 	print (msg2)
 	$VBoxContainer/Msg01.text = msg1
